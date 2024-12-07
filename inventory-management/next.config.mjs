ce089563bcc24e3a8116f306da725d5dceb76417
ritesh <ritesh25033@gmail.com>
2024-12-07 15:08:27 +0530

    sidebar navbar server

---
+++ inventory-management/next.config.mjs
@@ -0,0 +1,14 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
---
+++ inventory-management/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+        dashboard
+    </div>
+  )
+}
+
+export default Dashboard
