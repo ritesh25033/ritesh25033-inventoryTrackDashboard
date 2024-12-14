7a0367d67f1fb093289747cf69721664710e913d
ritesh <ritesh25033@gmail.com>
2024-12-14 12:43:51 +0530

    final commit

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -11,9 +11,9 @@ const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 /* ROUTE IMPORTS */
 const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
-// import productRoutes from "./routes/productRoutes";
-// import userRoutes from "./routes/userRoutes";
-// import expenseRoutes from "./routes/expenseRoutes";
+const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const expenseRoutes_1 = __importDefault(require("./routes/expenseRoutes"));
 /* CONFIGURATIONS */
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -23,14 +23,19 @@ app.use(helmet_1.default.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use((0, morgan_1.default)("common"));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
-app.use((0, cors_1.default)());
+// app.use(cors());
+app.use((0, cors_1.default)({
+    origin: "http://localhost:3000", // Replace with your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 /* ROUTES */
 app.use("/dashboard", dashboardRoutes_1.default); // http://localhost:8000/dashboard
-// app.use("/products", productRoutes); // http://localhost:8000/products
-// app.use("/users", userRoutes); // http://localhost:8000/users
-// app.use("/expenses", expenseRoutes); // http://localhost:8000/expenses
+app.use("/products", productRoutes_1.default); // http://localhost:8000/products
+app.use("/users", userRoutes_1.default); // http://localhost:8000/users
+app.use("/expenses", expenseRoutes_1.default); // http://localhost:8000/expenses
 /* SERVER */
-const port = Number(process.env.PORT) || 3001;
+const port = Number(process.env.PORT) || 8000;
 app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on port ${port}`);
 });
